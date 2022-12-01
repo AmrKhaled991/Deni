a8bcd870e4148289e74de66e5fedcff30727cfd8
iIslami <Amrkhaled033@gmail.com>
2022-12-01 18:13:57 +0200

    added splash quran and sura screen details

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:untitled/home_screen.dart';
+import 'package:untitled/suraname-details.dart';
+import 'package:untitled/themedata.dart';
 
 void main() {
   runApp(Myapp());
@@ -9,8 +11,12 @@ class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return MaterialApp(initialRoute: Homescreen.routname, routes: {
-      Homescreen.routname: (context) => Homescreen(),
-    });
+    return MaterialApp(
+        theme: Themedata.lighttheme,
+        routes: {
+          Homescreen.routname: (context) => Homescreen(),
+          Sura_detals.routname: (context) => Sura_detals()
+        },
+        initialRoute: Homescreen.routname);
   }
 }
