3839fdab9e3eb5f4f41b545daeeaa7c38b41c990
iIslami <Amrkhaled033@gmail.com>
2022-12-06 23:33:27 +0200

    app finsh

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:untitled/abbscreens/7adeth/hadeth_detals.dart';
 import 'package:untitled/home_screen.dart';
 import 'package:untitled/abbscreens/quran/suraname-details.dart';
+import 'package:untitled/provider/abbconfigprovider.dart';
 import 'package:untitled/themedata.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
-  runApp(Myapp());
+  runApp(ChangeNotifierProvider(
+    child: Myapp(),
+    create: (context) => Appconfigprovider(),
+  ));
 }
 
 class Myapp extends StatelessWidget {
-  @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    var provider = Provider.of<Appconfigprovider>(context);
     return MaterialApp(
         theme: Themedata.lighttheme,
+        darkTheme: Themedata.darkthem,
         routes: {
           Homescreen.routname: (context) => Homescreen(),
           Sura_detals.routname: (context) => Sura_detals(),
           Hadethdetails.routname: (context) => Hadethdetails()
         },
-        initialRoute: Homescreen.routname);
+        initialRoute: Homescreen.routname,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        locale: Locale(provider.languge),
+        themeMode: provider.apptheme);
   }
 }
--- lib/themedata.dart
+++ lib/themedata.dart
@@ -6,6 +6,8 @@ class Themedata {
   static Color primarylight = Color(0xffB7935F);
   static Color blackcolor = Color(0xff242424);
   static Color whitecolor = Colors.white;
+  static Color primarydark = Color(0xff0F1525);
+  static Color yellowcolor = Color(0xffFACC1D);
   static ThemeData lighttheme = ThemeData(
     primaryColor: primarylight,
     scaffoldBackgroundColor: Colors.transparent,
@@ -14,7 +16,7 @@ class Themedata {
         backgroundColor: Colors.transparent,
         centerTitle: true,
         iconTheme: IconThemeData(color: blackcolor)),
-    textTheme: TextTheme(
+    primaryTextTheme: TextTheme(
         headline1: TextStyle(
             color: blackcolor, fontWeight: FontWeight.bold, fontSize: 30),
         headline2: TextStyle(
@@ -26,4 +28,23 @@ class Themedata {
       unselectedItemColor: whitecolor,
     ),
   );
+  static ThemeData darkthem = ThemeData(
+      scaffoldBackgroundColor: Colors.transparent,
+      primaryColor: primarydark,
+      appBarTheme: AppBarTheme(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          centerTitle: true,
+          iconTheme: IconThemeData(color: whitecolor)),
+      primaryTextTheme: TextTheme(
+          headline1: TextStyle(
+              color: whitecolor, fontWeight: FontWeight.bold, fontSize: 30),
+          headline2: TextStyle(
+              color: whitecolor, fontWeight: FontWeight.w400, fontSize: 25),
+          headline3: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25)),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        selectedItemColor: yellowcolor,
+        unselectedItemColor: whitecolor,
+      ));
 }
